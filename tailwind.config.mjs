3918d9880862ba6ea5f5b7dbab29ea6de78ab422
Leeroy Garcia <leeroy.uziel.gg@outlook.es>
2025-01-08 13:18:27 -0600

    show progress

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'greatVibes': ['Great Vibes', 'cursive'],
+				'cinzelDecorative': ['Cinzel Decorative', 'system-ui'],
+				'playfairDisplay': ['Playfair Display Variable', 'serif'],
+			},
+			backgroundImage: {
+				'marfil': 'linear-gradient(to bottom, #ddd4c0, #e5dece, #eee8dc, #f6f3ea, #fffdf8)',
+			},
+		},
+	},
+	plugins: [],
+}
